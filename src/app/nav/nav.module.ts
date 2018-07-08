4929ca40242527776a4531229ff54e9e012e7684
Alex Fallenstedt <a.fallenstedt@gmail.com>
2018-07-07 22:51:59 -0700

    add overlay module

--- src/app/nav/nav.module.ts
+++ src/app/nav/nav.module.ts
@@ -3,10 +3,16 @@ import { CommonModule } from "@angular/common";
 import { NavContainerComponent } from "./nav-container/nav-container.component";
 import { BurgerComponent } from "./burger/burger.component";
 import { LogoComponent } from "./logo/logo.component";
+import { NavOverlayComponent } from "./nav-overlay/nav-overlay.component";
 
 @NgModule({
   imports: [CommonModule],
   exports: [NavContainerComponent],
-  declarations: [NavContainerComponent, BurgerComponent, LogoComponent]
+  declarations: [
+    NavContainerComponent,
+    BurgerComponent,
+    LogoComponent,
+    NavOverlayComponent
+  ]
 })
 export class NavModule {}
