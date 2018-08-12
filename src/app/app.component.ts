593c77733f65a2afc83fbbf92d916313e4462aaa
Alex Fallenstedt <a.fallenstedt@gmail.com>
2018-08-12 08:42:02 -0700

    ts warning fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,9 +6,7 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./app.component.scss"]
 })
 export class AppComponent implements OnInit {
-  title = "app";
-
-  constructor() {}
+  ngOnInit() {}
 
   get isHomePage() {
     return !(window.location.pathname === "/");
