f22f8418ec1e25ec65bb36ea53bcc5dfebd256d8
Alex Fallenstedt <a.fallenstedt@gmail.com>
2018-08-12 08:41:19 -0700

    hide footer on home

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,5 +8,9 @@ import { Component, OnInit } from "@angular/core";
 export class AppComponent implements OnInit {
   title = "app";
 
-  ngOnInit() {}
+  constructor() {}
+
+  get isHomePage() {
+    return !(window.location.pathname === "/");
+  }
 }
