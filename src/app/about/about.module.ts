095da34940eb0d1ac364f3df70cea6904bbcb8eb
Alex Fallenstedt <a.fallenstedt@gmail.com>
2018-08-11 21:08:01 -0700

    nuke transition update about page

--- src/app/about/about.module.ts
+++ src/app/about/about.module.ts
@@ -3,10 +3,9 @@ import { CommonModule } from "@angular/common";
 
 import { AboutRoutingModule } from "./about-routing.module";
 import { AboutContainerComponent } from "./about-container/about-container.component";
-import { SharedModule } from "../shared/shared.module";
 
 @NgModule({
-  imports: [CommonModule, AboutRoutingModule, SharedModule],
+  imports: [CommonModule, AboutRoutingModule],
   declarations: [AboutContainerComponent]
 })
 export class AboutModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgtUniversalModule } from "@ng-toolkit/universal";
 import { CommonModule } from "@angular/common";
-import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { AppRoutingModule } from "./app-routing.module";
 import { NavModule } from "./nav/nav.module";
