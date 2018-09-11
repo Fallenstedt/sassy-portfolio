dc63bfff76ddfff6a47ad9e8e3d996a2c4093b83
Alex Fallenstedt <a.fallenstedt@gmail.com>
2018-09-11 14:18:53 -0700

    add page transitions

--- layouts/default.vue
+++ layouts/default.vue
@@ -13,3 +13,15 @@ export default {
   }
 };
 </script>
+<style lang="scss">
+.page-enter-active,
+.page-leave-active {
+  transition: all 0.2s ease-out;
+}
+.page-enter,
+.page-leave-active {
+  opacity: 0;
+  transform-origin: 50% 50%;
+}
+</style>
+
