f036bb03fa05eec2c155da661add7c623768f1f9
Alex Fallenstedt <a.fallenstedt@gmail.com>
2018-09-11 14:58:37 -0700

    correct site title

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,7 +3,7 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: "alex-fallenstedt-portfolio",
+    title: "Alex Fallenstedt",
     meta: [
       { charset: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
