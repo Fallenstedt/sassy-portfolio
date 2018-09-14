a193906349e83297be24d5fa6477a6d03da41989
Alex Fallenstedt <a.fallenstedt@gmail.com>
2018-09-14 16:05:40 -0700

    add caption

---
+++ components/Caption.vue
@@ -0,0 +1,16 @@
+<template>
+    <p class="half-width"
+        :class="{ headline: isHeadline ? isHeadline : false }">
+      <slot></slot>
+    </p>
+</template>
+
+<script>
+export default {
+  name: "Caption",
+  props: ["isHeadline"]
+};
+</script>
+
+
+
