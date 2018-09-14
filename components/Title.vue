1147dad21894b5c40f39988271ee22b7044cbba8
Alex Fallenstedt <a.fallenstedt@gmail.com>
2018-09-14 16:06:13 -0700

    add title component

---
+++ components/Title.vue
@@ -0,0 +1,27 @@
+<template>
+  <h1><span class="title-container__bg-span"><slot></slot></span></h1>
+</template>
+
+<script>
+export default {
+  name: "Title"
+};
+</script>
+
+<style lang="scss">
+@import "assets/app.variables.scss";
+.title-container {
+  padding-top: $navHeight * 1.5;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  &__bg-span {
+    background-image: url("/5.jpg");
+    background-size: cover;
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+  }
+}
+</style>
+
+
