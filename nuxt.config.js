4457e3666b927c2bdaf0887103ecaf355d213420
Alex Fallenstedt <a.fallenstedt@gmail.com>
2019-01-27 19:49:20 -0800

    add compressed lazy loader

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,6 +5,7 @@ module.exports = {
    */
   head: {
     title: 'Alex Fallenstedt',
+    script: [{ src: '/js/yall.min.js' }],
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
