2ff5dd4e4c4a4ec5f9406d8e045d45d54618de9d
Alex Fallenstedt <a.fallenstedt@gmail.com>
2019-02-02 22:08:00 -0800

    add blog posts

--- blogs/index.js
+++ blogs/index.js
@@ -1,15 +1,28 @@
 const http = require('http');
+const fs = require('fs');
+const path = require('path');
+const writeLocation = path.resolve(__dirname, '..', 'static');
 const url = 'http://34.217.72.149/wp-json/wp/v2/posts';
 
 function main() {
-  http.get(url, resp => {
+  http.get(url, handleResponse());
+}
+function handleResponse() {
+  return resp => {
     let data = '';
     resp.on('data', chunk => {
       data += chunk;
     });
     resp.on('end', () => {
-      console.log(data);
+      writeBlogPosts(JSON.parse(data));
     });
-  });
+  };
 }
+
+function writeBlogPosts(posts) {
+  console.log('writing posts...');
+  fs.writeFileSync(`${writeLocation}/posts.json`, JSON.stringify(posts));
+  console.log('done writing posts!');
+}
+
 main();
