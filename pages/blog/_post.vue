911112b9e72bfc2d7e70fd07d9a3037e12920b4d
Alex Fallenstedt <a.fallenstedt@gmail.com>
2019-02-03 11:03:22 -0800

    show individual blog posts with correct slug and content!

---
+++ pages/blog/_post.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="post-container">
+    <div class="container">
+      <Title>{{post.title.rendered}}</Title>
+      <HorizontalBar></HorizontalBar>
+      <p v-html="post.content.rendered"></p>
+    </div>
+  </div>
+</template>
+
+<script>
+import Title from "../../components/Title.vue";
+import Caption from "../../components/Caption.vue";
+import HorizontalBar from "../../components/HorizontalBar.vue";
+import posts from "static/posts.json";
+export default {
+  components: {
+    Title,
+    Caption,
+    HorizontalBar
+  },
+  data() {
+    return {
+      post: posts[this.$route.params.post]
+    };
+  },
+  methods: {
+    getDate(date) {
+      const d = new Date(date);
+      return `${d.getMonth()}.${d.getDate()}.${d.getFullYear()}`;
+    },
+    getLink(slug) {
+      return `/blog/${slug}`;
+    }
+  },
+  mounted() {
+    console.log("hi");
+    console.log(this.post);
+  }
+};
+</script>
+
+<style lang="scss">
+@import "assets/app.variables.scss";
+.post-container {
+  padding-top: $navHeight * 1.5;
+}
+
+.posts {
+  &__post-title {
+    font-size: 1px;
+  }
+}
+</style>
+
--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -5,22 +5,22 @@
       <Caption>My random thoughts and learnings</Caption>
       <HorizontalBar></HorizontalBar>
       <div class="posts" v-for="(post, index) in posts" :key="index">
-        <a href>
+        <nuxt-link :to="getLink(post.slug)">
           <h1 class="headline" v-html="post.title.rendered"></h1>
           <p>{{getDate(post.date)}}</p>
           <no-ssr placeholder="Loading...">
             <p v-html="post.excerpt.rendered"></p>
           </no-ssr>
-        </a>
+        </nuxt-link>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import Title from "../components/Title.vue";
-import Caption from "../components/Caption.vue";
-import HorizontalBar from "../components/HorizontalBar.vue";
+import Title from "../../components/Title.vue";
+import Caption from "../../components/Caption.vue";
+import HorizontalBar from "../../components/HorizontalBar.vue";
 import posts from "static/posts.json";
 export default {
   components: {
@@ -37,6 +37,9 @@ export default {
     getDate(date) {
       const d = new Date(date);
       return `${d.getMonth()}.${d.getDate()}.${d.getFullYear()}`;
+    },
+    getLink(slug) {
+      return `/blog/${slug}`;
     }
   },
   mounted() {
