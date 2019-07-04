045993d0164f17dfb0e6e523b2c5d444c4de47f8
Alex Fallenstedt <a.fallenstedt@gmail.com>
2019-07-04 09:00:47 -0700

    test: 💍 rebuild nav
    * its a web component wow

--- static/js/main.js
+++ static/js/main.js
@@ -1,12 +1,72 @@
-//toggle nav bar
-(function() {
-  const open = document.getElementById("nav-open");
-  const close = document.getElementById("nav-close");
-  const navMobile = document.getElementById("nav-mobile");
-  if (close && close.onclick == null) {
-    close.onclick = () => navMobile.classList.add("hide");
-  }
-  if (open && open.onclick == null) {
-    open.onclick = () => navMobile.classList.remove("hide");
-  }
+(function createNavBar() {
+  const ATTR = {
+    IS_NAV_OPEN: "mobile-nav-open"
+  };
+  window.customElements.define(
+    "app-nav",
+    class extends HTMLElement {
+      static get observedAttributes() {
+        return [ATTR.IS_NAV_OPEN];
+      }
+
+      get open() {
+        return this.hasAttribute(ATTR.IS_NAV_OPEN);
+      }
+      set open(val) {
+        // Reflect the value of open as HTML attribute
+        if (val) {
+          this.setAttribute(ATTR.IS_NAV_OPEN, val);
+        } else {
+          this.removeAttribute(ATTR.IS_NAV_OPEN);
+        }
+      }
+
+      constructor() {
+        super();
+        this._createShadowDom();
+
+        this.openNavButton = this.shadowRoot.getElementById("nav-open");
+        this.closeNavButton = this.shadowRoot.getElementById("nav-close");
+
+        this._addEventListeners();
+      }
+
+      attributeChangedCallback(name, oldValue, newValue) {
+        if (ATTR.IS_NAV_OPEN) {
+          const overlay = this.shadowRoot.querySelector(".nav-overlay");
+          this._toggleOverlay(newValue, overlay);
+        }
+      }
+
+      toggleNav() {
+        if (this.open) {
+          this.open = undefined;
+        } else {
+          this.open = "true";
+        }
+      }
+
+      _addEventListeners() {
+        this.closeNavButton.addEventListener(
+          "click",
+          this.toggleNav.bind(this)
+        );
+        this.openNavButton.addEventListener("click", this.toggleNav.bind(this));
+      }
+
+      _createShadowDom() {
+        this.template = document.getElementById("app-nav");
+        this.attachShadow({ mode: "open" }).appendChild(
+          this.template.content.cloneNode(true)
+        );
+      }
+      _toggleOverlay(newValue, overlay) {
+        if (newValue == "true") {
+          overlay.classList.remove("hide");
+        } else {
+          overlay.classList.add("hide");
+        }
+      }
+    }
+  );
 })();
