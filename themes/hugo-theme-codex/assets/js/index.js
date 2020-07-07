a214543ee8cb56827d37029e2403d64ae602feb3
Alex Fallenstedt <a.fallenstedt@gmail.com>
2020-07-06 21:37:37 -0700

    woops

--- themes/hugo-theme-codex/assets/js/index.js
+++ themes/hugo-theme-codex/assets/js/index.js
@@ -1,19 +1,32 @@
-/*
- * Handles mobile nav
- */
 
-function toggleMobileNavState() {
-  const body = document.querySelector("body");
-  body.classList.toggle("nav--active");
-}
+initBurger();
+
+getMyLatestTweet()
+
 
 /*
  * Initializes burger functionality
  */
 
 function initBurger() {
+  function toggleMobileNavState() {
+    const body = document.querySelector("body");
+    body.classList.toggle("nav--active");
+  }
   const burger = document.querySelector(".burger");
   burger.addEventListener("click", toggleMobileNavState);
 }
 
-initBurger();
+
+/**
+ * Fetches latest tweet
+ */
+async function getMyLatestTweet() {
+  const resp = await window.fetch("https://yi6fgzh27l.execute-api.us-west-2.amazonaws.com/Prod/tweet");
+  if (resp.ok) {
+    const data = await resp.json()
+    const tweetContainer = document.getElementById("latest_tweet");
+    tweetContainer.innerText = data.message;
+  }
+}
+
